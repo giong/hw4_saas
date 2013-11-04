f45e323211db8c98c8678a4620e5871e34d3300f
NGUYEN NGOC LAN <lan.startup.stanford@gmail.com>
2013-11-04 10:57:05 +0700

    add unit test for model

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Movie do
+  fixtures :movies
+  describe 'searching tmdb by director' do
+    it 'should call find Movie by id' do
+      fake_result = double('Movie', :director => 'George Lucas')
+      Movie.should_receive(:find).with(1).and_return(fake_result)
+      Movie.find_with_similar_director(1)
+    end
+    it 'should call find Movie by director' do
+      Movie.should_receive(:where).with('George Lucas')
+      Movie.find_with_similar_director(1)
+    end
+  end
+end
